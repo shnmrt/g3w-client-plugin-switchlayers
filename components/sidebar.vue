20e3e16382116f690d148fc47cff078e19ef32c2
Raruto <Raruto@users.noreply.github.com>
2022-07-20 08:38:10 +0200

    remove additional paragraph

--- components/sidebar.vue
+++ components/sidebar.vue
@@ -2,7 +2,6 @@
     <ul id="<PLUGIN_ID>" class="treeview-menu">
         <li>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus ultricies tristique nulla aliquet enim tortor at auctor. Aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices. Quis imperdiet massa tincidunt nunc pulvinar sapien. Leo vel orci porta non pulvinar. Odio euismod lacinia at quis. Congue eu consequat ac felis donec et. Pulvinar sapien et ligula ullamcorper malesuada proin libero. Suspendisse sed nisi lacus sed viverra tellus in. Dictumst quisque sagittis purus sit amet volutpat consequat mauris nunc. Purus sit amet volutpat consequat mauris nunc congue.</p>
-            <p>Lacus sed turpis tincidunt id aliquet risus feugiat in. Ultricies integer quis auctor elit sed vulputate mi sit amet. At volutpat diam ut venenatis tellus. Senectus et netus et malesuada fames ac turpis egestas. Ac felis donec et odio pellentesque. Odio tempor orci dapibus ultrices in iaculis nunc. Donec adipiscing tristique risus nec feugiat in. Urna nunc id cursus metus aliquam eleifend mi. Sollicitudin nibh sit amet commodo nulla facilisi nullam. Nam libero justo laoreet sit amet cursus sit. Platea dictumst vestibulum rhoncus est pellentesque. Congue nisi vitae suscipit tellus mauris a diam. Eu ultrices vitae auctor eu augue ut lectus arcu bibendum. Lobortis elementum nibh tellus molestie nunc non. Eleifend donec pretium vulputate sapien.</p>
         </li>
     </ul>
 </template>
