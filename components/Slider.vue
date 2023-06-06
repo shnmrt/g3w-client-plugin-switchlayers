74b27929033f38afca66df19825d1243da7879ac
shnmrt <sahinmurat3@gmail.com>
2023-06-06 11:48:22 +0200

    create slider for layers

---
+++ components/Slider.vue
@@ -0,0 +1,44 @@
+<template>
+    <form>
+        <div class="form-group">
+            <input 
+                type="range" 
+                class="form-control-range"
+                id="slider"
+                min="1"
+                :max="groupsize"
+                v-model="sliderValue"
+                @input="sliderEvent($event)"
+                >
+        </div>
+    </form>
+</template>
+
+<script>
+
+export default {
+    name:'Slider',
+    props:{
+        groupsize : Number,
+        slidercurrentValue: Number,
+    },
+    data: function() {
+        return {
+            sliderValue: 1
+        }
+    },
+    methods: {
+        sliderEvent: function() {
+            console.log(this.sliderValue)
+            this.$emit('slider-change',this.sliderValue)
+        }
+    }
+}
+</script>
+
+<style scoped>
+form{
+    margin-left: 10px;
+    margin-right: 10px;
+}
+</style>
