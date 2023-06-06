77eacbaf602b1eb29468157e166c9fc39af41f23
shnmrt <sahinmurat3@gmail.com>
2023-06-06 11:47:18 +0200

    create component for layer names

---
+++ components/Layer.vue
@@ -0,0 +1,23 @@
+<template>
+    <p>{{ layertitle }}</p>
+</template>
+
+<script>
+export default {
+    name: 'Layer',
+    props:{
+        layertitle: String
+    }
+}
+</script>
+
+<style scoped>
+ p {
+    color: white;
+    margin-left: 10px;
+    margin-right: 10px;
+    font-size: 1.65rem;
+    align-content: right ;
+    align-self: center;
+ }
+</style>
