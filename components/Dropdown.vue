326b0d73bfef860148257aeb8eb7fa485193e126
shnmrt <sahinmurat3@gmail.com>
2023-06-06 11:47:56 +0200

    create dropdown component for groups

---
+++ components/Dropdown.vue
@@ -0,0 +1,38 @@
+<template>
+    <select class="dropdown col btn btn-primary dropdown-toggle"
+    v-model="key"
+    @change="onChange($event)"
+    >
+        <option value="" selected disabled>Select a Group</option>
+        <option
+            v-for="(layer, index) in layerstree" 
+            :value='index'
+            :key="index"         
+            >{{ layer.title }}</option>
+    </select>
+</template>
+
+<script>
+export default {
+    name: 'Dropdown',
+    props:["layerstree"],
+    data() {
+        return {
+            key: ''
+        }
+    },
+    methods: {
+        onChange(event) {
+            const groupID = event.target.value;
+            this.$emit('set-group', groupID);
+        }
+    }
+}
+</script>
+
+<style scoped>
+.dropdown {
+    margin-top: 10px;
+    margin-bottom: 20px    
+}
+</style>
